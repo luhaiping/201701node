56ca0617437b78a36bdc6a905aabb47ffd92af1b
zhufengnodejs <zhufengnodejs@126.com>
2017-05-28 10:26:25 +0800

    es6 class

--- 8.websocket/1.query.js
+++ 8.websocket/1.query.js
@@ -1,7 +1,25 @@
 let express = require('express');
 let app = express();
+let startTime = Date.now();
 app.get('/clock',function(req,res){
   res.setHeader('Access-Control-Allow-Origin','http://localhost:63342');
   res.send(new Date().toLocaleString());
+  console.log(Date.now() - startTime);
+    startTime = Date.now();
+});
+let path = require('path');
+app.get('/',function(req,res){
+  res.sendFile(path.resolve('./3.iframe.html'));
+});
+app.get('/clock2',function(req,res){
+  setInterval(function(){
+      let html = `
+  <script>
+    window.parent.changeTime('${new Date().toLocaleString()}')
+    </script>
+  `;
+      res.write(html);
+  },1000)
+  //res.send(html); //end
 });
 app.listen(8080);
