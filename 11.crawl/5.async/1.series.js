7d6c7d79ccab4f2e1854ceb5612265aa2b918c9a
zhufengnodejs <zhufengnodejs@126.com>
2017-05-29 16:39:49 +0800

    cheerio

--- 11.crawl/5.async/1.series.js
+++ 11.crawl/5.async/1.series.js
@@ -4,34 +4,38 @@
 let async = require('async');
 console.time('cost')
 //数组中的异步任务会依次执行
-function series(tasks,finalCallback){
-  let index =0;
-  function next(){
-      tasks[index++](next);
-  }
-  next();
+function series(tasks, finalCallback) {
+    let index = 0;
+    let ary = [];
+
+    function next(err, result) {
+        index > 0 ? ary.push(result) : null;
+        (err || index >= tasks.length) ? finalCallback(err, ary) : tasks[index++](next);
+    }
+
+    next();
 }
 series([
-    function(next){
-       setTimeout(function(){
-           console.log(1);
-           // 1 错误对象
-           next(null,1);
-       },3000)
+    function (next) {
+        setTimeout(function () {
+            console.log(1);
+            // 1 错误对象
+            next(null, 1);
+        }, 3000)
     },
-    function(next){
-        setTimeout(function(){
+    function (next) {
+        setTimeout(function () {
             console.log(2);
-            next('错误',2);
-        },2000)
+            next(null, 2);
+        }, 2000)
     },
-    function(next){
-        setTimeout(function(){
+    function (next) {
+        setTimeout(function () {
             console.log(3);
-            next(null,3);
-        },1000)
+            next(null, 3);
+        }, 1000)
     }
-],function(err,result){
+], function (err, result) {
     console.log(err);
     console.log(result);
     console.timeEnd('cost');
